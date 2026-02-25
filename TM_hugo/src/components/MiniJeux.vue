6a67cf4f79e1e65637193a9d5e489a1ba00ab1eb
hugohausammann <px07egl@eduvaud.ch>
2026-02-25 15:49:28 +0100

    mise au point 25.02.26

---
+++ TM_hugo/src/components/MiniJeux.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const compteur = ref(0);
+
+const trop=computed(() => {
+  return compteur.value > 10;
+});
+
+
+function alert() {
+compteur.value++;
+}
+</script>
+
+<template>
+  <div class="mini-jeux">
+    <h2 class="text-2xl">Mini Jeux</h2>
+    <p>Bienvenue dans la section des mini jeux !</p>
+    <!-- Ajoutez ici vos mini jeux -->
+<button 
+class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+@click="alert()">Mini Jeu {{ compteur }}</button>
+<p v-if="trop">Trop de clics !</p>
+</div>
+</template>
+
